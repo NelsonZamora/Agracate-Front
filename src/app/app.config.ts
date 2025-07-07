681d6ec18c156267815b8779e47046d55b9ab777
unknown <nelsonzhingri@gmail.com>
2025-07-07 14:49:36 -0500

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter,Routes } from '@angular/router';
 
-import { routes } from './app.routes';
+// import { routes } from './app.routes';
+
+import { Monitoreo } from './monitoreo/monitoreo';
+import { Inventario } from './inventario/inventario';
+import { Informes } from './informes/informes';
+import { Navbar } from './navbar/navbar';
+
+const routes: Routes = [
+  { path: 'Monitoreo', component: Monitoreo },
+  { path: 'Inventario', component: Inventario },
+  { path: 'Informes', component: Informes }
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [
